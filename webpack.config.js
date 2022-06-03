eb2495947d0e4ec7b93e09f10720f3da7804bff8
Josh <josue.delacruz@americavoice.com>
2022-06-03 11:05:12 -0600

    Add placeholder variables

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 
 module.exports = {
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+        hash: true,
+        title: 'Webpack Starter',
+        header: 'Webpack Starter',
+        metaDesc: 'Basic webpack configuration for HTML and SASS',
+        template: './src/index.html',
+        inject: 'body',
+    })
   ]
 };
