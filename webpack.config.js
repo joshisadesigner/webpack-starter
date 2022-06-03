af2c61fed0e1d91adf19dc9974913c548072bdc2
Josh <josue.delacruz@americavoice.com>
2022-06-03 11:05:12 -0600

    Add dist directory clean

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-        hash: true,
-        title: 'Webpack Starter',
-        header: 'Webpack Starter',
-        metaDesc: 'Basic webpack configuration for HTML and SASS',
-        template: './src/index.html',
-        inject: 'body',
-    })
-  ]
+    mode: 'development',
+    output: {
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Webpack Starter',
+            header: 'Webpack Starter',
+            metaDesc: 'Basic webpack configuration for HTML and SASS',
+            template: './src/index.html',
+            inject: 'body',
+        })
+    ]
 };
